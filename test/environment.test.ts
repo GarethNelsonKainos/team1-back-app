d9796f6e4e4002cc80a51ef7a88f3c208499b48e
MatthewDobsonKainos <matthew.dobson@kainos.com>
2026-02-12 16:04:17 +0000

    Changed PrismaClient path import.

--- test/environment.test.ts
+++ test/environment.test.ts
@@ -1,7 +1,7 @@
+import type { PrismaClient } from '@prisma/client';
 import type { Express } from 'express';
 import request from 'supertest';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import type { PrismaClient } from '../src/generated/prisma/client';
 
 describe('Application Environment Configuration', () => {
   let app: Express;
