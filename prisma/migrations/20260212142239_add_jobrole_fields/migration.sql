090d36adedd8fff151e20a6f650bbade260958d7
chrisforeman1882 <chris.foreman@kainos.com>
2026-02-13 15:10:27 +0000

    update README

--- prisma/migrations/20260212142239_add_jobrole_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "JobRole" ADD COLUMN     "description" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "numberOfOpenPositions" INTEGER NOT NULL DEFAULT 1,
-ADD COLUMN     "responsibilities" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "sharepointUrl" TEXT NOT NULL DEFAULT '';
