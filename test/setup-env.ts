04631df824e72e7d4b3578fc9e4d9f9ddb323f9c
chrisforeman1882 <chris.foreman@kainos.com>
2026-02-26 15:15:56 +0000

    some more setup-env vars

--- test/setup-env.ts
+++ test/setup-env.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.test' });
 
+
 process.env.DB_USER = process.env.DB_USER || 'postgres';
 process.env.DB_HOST = process.env.DB_HOST || 'localhost';
 process.env.DB_PORT = process.env.DB_PORT || '5432';
@@ -16,3 +17,8 @@ process.env.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '15m';
 process.env.BCRYPT_SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS || '10';
 process.env.REFRESH_TOKEN_EXPIRES_IN =
   process.env.REFRESH_TOKEN_EXPIRES_IN || '7d';
+
+process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || 'test-access-key-id';
+process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || 'test-secret-access-key';
+process.env.AWS_REGION = process.env.AWS_REGION || 'eu-west-1';
+process.env.S3_BUCKET_NAME = process.env.S3_BUCKET_NAME || 'test-bucket';
